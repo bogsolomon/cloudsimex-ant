ae8e73080ccc4a8cc08395427f19a7b858151878
bogsolomon <bog.solomon@gmail.com>
2017-02-17 17:04:51 -0500

    Updated grapher logic

--- src/main/java/ca/ncct/uottawa/selforg/ant/sim/logparser/Grapher.java
+++ src/main/java/ca/ncct/uottawa/selforg/ant/sim/logparser/Grapher.java
@@ -1,6 +1,7 @@
 package ca.ncct.uottawa.selforg.ant.sim.logparser;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,13 +13,18 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.axis.NumberTickUnit;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.renderer.category.CategoryItemRenderer;
-import org.jfree.chart.renderer.category.LineAndShapeRenderer;
-import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYSplineRenderer;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 public class Grapher {
+
+  public static final int MAX_GRAPH_TIME = 6 * 60 * 60;
+
   public static void main(String[] args) throws Exception {
     File[] files = new File(args[0]).listFiles(file -> file.isFile() && file.toString().endsWith(".out"));
 
@@ -30,38 +36,65 @@ public class Grapher {
   private static void makeGraph(File f) throws IOException {
     Path path = f.toPath();
     String basename = FilenameUtils.getBaseName(path.getFileName().toString());
-    Path outPath = path.getParent().resolve(basename+".png");
-
     List<String> lines = Files.readAllLines(path);
     lines.remove(0);
-    DefaultCategoryDataset cpuDataset = new DefaultCategoryDataset();
-    DefaultCategoryDataset sessionsDataset = new DefaultCategoryDataset();
+
+    XYSeries cpuSeries = new XYSeries("CPU usage");
+    XYSeries sessionsSeries = new XYSeries("Sessions");
+
+    int imageCount = 1;
+    double maxTime = MAX_GRAPH_TIME;
     for (String line : lines) {
       String[] vals = line.split(",");
+      Double time = Double.valueOf(vals[0]);
+      if (time > maxTime) {
+        writeChart(cpuSeries, sessionsSeries, basename, imageCount, path, maxTime);
+        maxTime += MAX_GRAPH_TIME;
+        imageCount++;
+        cpuSeries.clear();
+        sessionsSeries.clear();
+      }
 
-      cpuDataset.addValue(Double.valueOf(vals[2]), "CPU usage", Double.valueOf(vals[0]));
-      sessionsDataset.addValue(Double.valueOf(vals[3]), "Sessions", Double.valueOf(vals[0]));
+      cpuSeries.add(time, Double.valueOf(Double.valueOf(vals[2]) * 100));
+      sessionsSeries.add(time, Double.valueOf(vals[3]));
     }
 
-    JFreeChart lineChartObject = ChartFactory.createLineChart("CPU usage", "Time (s)", "CPU usage",
-        cpuDataset, PlotOrientation.VERTICAL, true, true, false);
-    CategoryPlot plot = (CategoryPlot) lineChartObject.getPlot();
-    plot.setDataset( 1, sessionsDataset );
+    // write final image
+    writeChart(cpuSeries, sessionsSeries, basename, imageCount, path, maxTime);
+  }
 
-    final NumberAxis axis2 = new NumberAxis("Session count");
-    axis2.setAutoRangeIncludesZero(false);
-    plot.setRangeAxis(1, axis2);
+  private static void writeChart(XYSeries cpuSeries, XYSeries sessionsSeries,
+      String basename, int imageCount, Path path, double maxTime) throws IOException {
+    XYSeriesCollection cpuDataset = new XYSeriesCollection();
+    XYSeriesCollection sessionsDataset = new XYSeriesCollection();
+    cpuDataset.addSeries(cpuSeries);
+    sessionsDataset.addSeries(sessionsSeries);
+
+    XYPlot plot = new XYPlot();
+    plot.setDataset(0, cpuDataset);
+    plot.setDataset(1, sessionsDataset);
+
+    XYSplineRenderer splinerendererCPU = new XYSplineRenderer();
+    splinerendererCPU.setBaseShapesVisible(false);
+    plot.setRenderer(0, splinerendererCPU);
+
+    XYSplineRenderer splinerenderer = new XYSplineRenderer();
+    splinerenderer.setSeriesFillPaint(0, Color.BLUE);
+    splinerenderer.setBaseShapesVisible(false);
+    plot.setRenderer(1, splinerenderer);
+    plot.setRangeAxis(0, new NumberAxis("CPU usage (%)"));
+    plot.setRangeAxis(1, new NumberAxis("Session count"));
+    plot.setDomainAxis(new NumberAxis("Time (s)"));
+    plot.getDomainAxis().setRange(maxTime - MAX_GRAPH_TIME, maxTime);
+
+    plot.mapDatasetToRangeAxis(0, 0);
     plot.mapDatasetToRangeAxis(1, 1);
-
-    LineAndShapeRenderer defaultRenderer = (LineAndShapeRenderer) plot.getRenderer(0);
-    LineAndShapeRenderer splineRenderer = new LineAndShapeRenderer();
-    splineRenderer.setSeriesPaint( 0, new Color( 191, 221, 255 ) );
-
-    plot.setRenderer( 1, defaultRenderer );
-    plot.setRenderer( 0, splineRenderer );
 
     int width = 640; /* Width of the image */
     int height = 480; /* Height of the image */
-    ChartUtilities.saveChartAsPNG(outPath.toFile(), lineChartObject, width, height);
+    Path outPath = path.getParent().resolve(basename + imageCount + ".png");
+
+    JFreeChart chart = new JFreeChart("Simulation Results", Font.getFont(Font.SANS_SERIF), plot, true);
+    ChartUtilities.saveChartAsPNG(outPath.toFile(), chart, width, height);
   }
 }
